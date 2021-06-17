0058d09a692d3bbd0d3fd685f2cdb6fc448c6385
K L Arjun <arjunkl07@gmail.com>
2021-06-17 15:45:06 -0500

    Updates

---
+++ precache-manifest.6f63497228224b83ecb89625b54d42ad.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90b8ab3dbff0292dd2a5f9fe3881bf9",
+    "url": "/klarjun.github.io/index.html"
+  },
+  {
+    "revision": "59acc8ce401fcff97255",
+    "url": "/klarjun.github.io/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "a535ac38222f1d79ede8",
+    "url": "/klarjun.github.io/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "55d1f592409dc6081b61",
+    "url": "/klarjun.github.io/static/css/main.c30bc0e0.chunk.css"
+  },
+  {
+    "revision": "9afdd8f842688af0a7c5",
+    "url": "/klarjun.github.io/static/js/2.bc4b0462.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/klarjun.github.io/static/js/2.bc4b0462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59acc8ce401fcff97255",
+    "url": "/klarjun.github.io/static/js/3.700adead.chunk.js"
+  },
+  {
+    "revision": "a535ac38222f1d79ede8",
+    "url": "/klarjun.github.io/static/js/4.6f00fa93.chunk.js"
+  },
+  {
+    "revision": "55d1f592409dc6081b61",
+    "url": "/klarjun.github.io/static/js/main.a7c195bf.chunk.js"
+  },
+  {
+    "revision": "8f87adff85266c027209",
+    "url": "/klarjun.github.io/static/js/runtime-main.916d853a.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/klarjun.github.io/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "0fa24081b3fba901f3943c7afce5b49f",
+    "url": "/klarjun.github.io/static/media/JavaScript.0fa24081.png"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/klarjun.github.io/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "e7829c23cd1b5e34bc4b8aaa015cd0a7",
+    "url": "/klarjun.github.io/static/media/React.e7829c23.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/klarjun.github.io/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "f7b35e27beb9140eac54b93114fa0523",
+    "url": "/klarjun.github.io/static/media/dataStructure.f7b35e27.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/klarjun.github.io/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e1c82a59f2b98cc4c876b3fc8beb9573",
+    "url": "/klarjun.github.io/static/media/freelance.e1c82a59.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/klarjun.github.io/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/klarjun.github.io/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/klarjun.github.io/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "542481d7829e350640aea4dca6f802c7",
+    "url": "/klarjun.github.io/static/media/uah.542481d7.png"
+  },
+  {
+    "revision": "21341203688d1024c2b4e646bbabdbe8",
+    "url": "/klarjun.github.io/static/media/vtu.21341203.png"
+  }
+]);
