455f51f3d2a9c3a903cea3672db65419cd0317ac
K L Arjun <arjunkl07@gmail.com>
2021-06-17 19:24:29 -0500

    Updates

--- precache-manifest.50a81e13db9870ace857380343b9ac65.js
+++ precache-manifest.50a81e13db9870ace857380343b9ac65.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "77e52243d4cba96895a6a7e3be83b0ba",
+    "revision": "82e7f2fe285c291f89d05b6de11daaaa",
     "url": "/portfolio/index.html"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.bd60c4cfddf5e30205c44e4aed4cab69.js"
+  "/portfolio/precache-manifest.50a81e13db9870ace857380343b9ac65.js"
 );
 
 self.addEventListener('message', (event) => {
